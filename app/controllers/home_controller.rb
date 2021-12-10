57b62119b9211e7af1c09d7baca464bac0c23216
Stefano Argentino <s.argentino@donboscosandona.it>
2021-12-10 10:42:58 +0000

    creazione home page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+    def index 
+        # questa azione non fa niente
+        # ma richiama la corrispondente
+        # vista index
+    end
+    
+end
